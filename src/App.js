6007791d57cfaaed4e93461d989193e6102bf9a4
Florian Chrometz <florian@chrometz.me>
2019-09-06 17:52:32 +0200

    Added check for devicetype

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import React from 'react';
+import {
+    BrowserView,
+    MobileView,
+    isBrowser,
+    isMobile
+} from "react-device-detect";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Container,Row,Col } from 'reactstrap';
 
 function App() {
-  return (
-    <div className="App">
-      <p>Test</p>
-    </div>
-  );
+    return (
+        <div className="App">
+            <BrowserView>
+                <Container>
+                    <Row>
+                        <Col>
+                            <p>Is Browser</p>
+                        </Col>
+                    </Row>
+                </Container>
+            </BrowserView>
+            <MobileView>
+                <Container>
+                    <Row>
+                        <Col>
+                            <p>This application is not available for mobile browsers, yet!</p>
+                        </Col>
+                    </Row>
+                </Container>
+            </MobileView>
+        </div>
+    );
 }
 
 export default App;
