77881b4c0e76e1c8ed018d123e0e6d04ecfc9257
Florian Chrometz <florian@chrometz.me>
2019-11-15 14:30:38 +0100

    Improvements for API handling and UI

--- src/App.js
+++ src/App.js
@@ -1,96 +1,111 @@
 import React from 'react';
-import {
-    BrowserView,
-    MobileView,
-    isBrowser,
-    isMobile
-} from "react-device-detect";
+import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css'
 import { Container,Row,Col,Button } from 'reactstrap';
+import clamp from 'lodash-es/clamp'
+import { useSpring, animated } from 'react-spring'
+import { useGesture } from 'react-with-gesture'
 
+const DEBUG_PRINT = false;
 const API_MQTTMSG_URL = "http://api-server-fk-sc.aotp012.mcs-paas.io/api/v1/publish/message";
+
+function Joystick() {
+    const [{ xy }, set] = useSpring(() => ({ xy: [0, 0] }))
+    const bind = useGesture(({ down, delta, velocity }) => {
+        velocity = clamp(velocity, 1, 8)
+        set({ xy: down ? delta : [0, 0], config: { mass: velocity, tension: 500 * velocity, friction: 50 } })
+    })
+    return <animated.div {...bind()} style={{ transform: xy.interpolate((x, y) => `translate3d(${x}px,${y}px,0)`) }} />
+}
+
 
 class App extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             carSpeed: 0,
-            carSteering: 0
+            carSteering: 0,
+            APISuccess:0,
+            APIFailed:0
         };
 
     }
 
+    postUpdateToApi = () => {
+        let carData = {speed:this.state.carSpeed,steering:this.state.carSteering}
+        axios.post(API_MQTTMSG_URL, carData, {headers: {'Content-Type': 'application/json'}})
+            .then(res => {
+                if(DEBUG_PRINT){
+                    console.log(res);
+                    console.log(res.data);
+                }
+                this.setState({APISuccess : this.state.APISuccess+1})
+            })
+            .catch(error => {
+                this.setState({APIFailed : this.state.APIFailed+1});
+                if(DEBUG_PRINT){
+                    console.log(error.response)
+                }
+            });
+    };
+
     //reset car values
-    resetCar = () => {
+    emergencyBreak = () => {
         this.setState({
             carSpeed: 0,
             carSteering: 0
-        })
-    }
+        }, () => this.postUpdateToApi())
+    };
 
     //handle car speed form events
     handleCarSpeed = (event) => {
         this.setState({
             carSpeed: event.target.value
-        })
-
-        var xhr = new XMLHttpRequest()
-
-        xhr.open('POST', API_MQTTMSG_URL);
-        xhr.setRequestHeader("Content-Type", "application/json");
-        xhr.send(JSON.stringify({ speed: event.target.value }))
-    }
+        },() => this.postUpdateToApi())
+    };
 
     //handle car steering form events
     handleCarSteering = (event) => {
         this.setState({
             carSteering: event.target.value
-        })
-
-        var xhr = new XMLHttpRequest()
-
-        xhr.open('POST', API_MQTTMSG_URL);
-        xhr.setRequestHeader("Content-Type", "application/json");
-        xhr.send(JSON.stringify({ steering: event.target.value }))
-
-    }
-
+        },() => this.postUpdateToApi())
+    };
 
     startRequestFloodWith = (numberOfRequests) => {
-
-        for (let i = 0; i < numberOfRequests; i++) {
-            var xhr = new XMLHttpRequest()
-            console.log("flood with: ", i)
-            xhr.open('POST', API_MQTTMSG_URL);
-            xhr.setRequestHeader("Content-Type", "application/json");
-            xhr.send(JSON.stringify({ speed: 0 }))
-        }
-
-
-    }
-
-
-
-    //subscribe to react state changes and send them to mqtt
-    static getDerivedStateFromProps(props, state) {
-        console.log(JSON.stringify(state))
-    }
+        this.setState({
+            carSpeed: 0,
+            carSteering: 0
+        },() => {
+            for (let i = 0; i < numberOfRequests; i++) {
+                this.postUpdateToApi()
+            }
+        })
+    };
 
     render(){
         return (
-        <div className="App">
-            <BrowserView >
-                <Container>
+            <div className="App">
+
+                <Container style={{height:"50vh", backgroundColor:"#eee"}} className={"joystickContainer"}>
+
+                    <Joystick/>
+
+                </Container>
+
+
+                <Container style={{height:"50vh"}}>
                     <Row>
                         <Col>
-                            <p>Is Browser</p>
+                            <p>Success API requests: {this.state.APISuccess}</p>
+                            <p>Failed API requests: {this.state.APIFailed}</p>
                         </Col>
                         <Col>
                             <p>Speed is: <b>{this.state.carSpeed}%</b></p>
                             <input
                                 id="speedInput"
                                 type="range"
-                                min="-90" max="90"
+                                min="-100" max="100"
                                 value={this.state.carSpeed}
                                 onChange={this.handleCarSpeed}
                                 step="1"/>
@@ -100,38 +115,35 @@ class App extends React.Component {
                             <input
                                 id="steeringInput"
                                 type="range"
-                                min="-90" max="90"
+                                min="-100" max="100"
                                 value={this.state.carSteering}
                                 onChange={this.handleCarSteering}
                                 step="1"/>
                         </Col>
-                    </Row>
-                </Container>
-                <Container>
-                    <Row>
                         <Col>
-                            <Button onClick={() => this.startRequestFloodWith(100)} >Send 100 Requests</Button>
-                        </Col>
-                        <Col>
-                            <Button onClick={() => this.startRequestFloodWith(1000)} >Send 1.000 Requests</Button>
-                        </Col>
-                        <Col>
-                            <Button onClick={() => this.startRequestFloodWith(10000)} >Send 10.000 Requests</Button>
-                        </Col>
-                    </Row>
-                </Container>
-            </BrowserView>
-            <MobileView>
-                <Container>
-                    <Row>
-                        <Col>
-                            <p>This application is not available for mobile browsers, yet!</p>
+                            <Row>
+                                <Col>
+                                    <Button style={{width:"100%"}} onClick={() => this.startRequestFloodWith(100)} >Send 100 Requests</Button>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col>
+                                    <Button style={{width:"100%"}} onClick={() => this.startRequestFloodWith(1000)} >Send 1.000 Requests</Button>
+                                </Col>
+                            </Row>
+                            <Row>
+                                <Col>
+                                    <Button style={{width:"100%"}} onClick={() => this.startRequestFloodWith(10000)} >Send 10.000 Requests</Button>
+                                </Col>
+                            </Row>
                         </Col>
                     </Row>
                 </Container>
-            </MobileView>
-        </div>
-    );
+
+
+
+            </div>
+        );
 
     }
 
